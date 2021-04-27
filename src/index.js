01f39c32d588c1c811eb107a1a05235e7a36ee0d
matthewjstahnke <matthewjstahnke@gmail.com>
2021-04-26 22:38:14 -0500

    set up redux store

--- src/index.js
+++ src/index.js
@@ -4,9 +4,29 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// allows us to set up our store, add thunk, add chrome dev tools
+import { createStore, compose, applyMiddleware} from 'redux'
+// Gives us redux
+import { Provider } from 'react-redux'
+// adds ability to make asynchronous actions for Redux
+import thunk from 'redux-thunk'
+// Gives us Router
+import { BrowserRouter as Router } from 'react-router-dom'
+// We build this:
+import { reducer } from './redux/reducer'
+
+const store = createStore(reducer, compose(
+  applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+)
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
